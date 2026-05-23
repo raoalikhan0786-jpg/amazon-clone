33d4057a5b183137df3b432c7ed4713fdc4a4403
raoalikhan0786-jpg <raoalikhan0786@gmail.com>
2026-05-23 23:05:56 +0530

    auto seed products

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 import express from "express";
-
 import dotenv from "dotenv";
-
 import cors from "cors";
 
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
-
 import productRoutes from "./routes/productRoutes.js";
-
 import orderRoutes from "./routes/orderRoutes.js";
+
+import Product from "./models/Product.js";
+import products from "./data/products.js";
 
 dotenv.config();
 
@@ -19,8 +18,36 @@ connectDB();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
+const seedProducts = async () => {
+
+    try {
+
+        const count =
+            await Product.countDocuments();
+
+        if (count === 0) {
+
+            await Product.insertMany(products);
+
+            console.log("Products Seeded");
+
+        } else {
+
+            console.log("Products Already Exist");
+
+        }
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+};
+
+seedProducts();
 
 app.get("/", (req, res) => {
 
